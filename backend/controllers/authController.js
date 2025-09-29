c882d43e2a24277d0d4d68ea46edb44ca015213b
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-09-29 23:52:08 +0545

    Updated Controller Function to User, Auth and Products

---
+++ backend/controllers/authController.js
@@ -0,0 +1,38 @@
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+
+//Sign Up
+const signUp = async(request, response) => {
+    const {username, name, password} = request.body
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    const user = new User({username, name, passwordHash})
+    const savedUser = await user.save()
+
+    response.status(201).json({message: 'SignUp Successful', userId: savedUser.id})
+}
+
+
+//Login
+const login = async(req, res) => {
+    const {username, password} = req.body
+
+    const user = await User.findOne({username})
+
+    if (!user) return res.status(401).json({error: 'Invalid Username'})
+    
+    const passwordCorrect = await bcrypt.compare(password, user.passwordHash)
+
+    if (!passwordCorrect) return res.status(401).json({error: 'Invalid Credentials'})
+    
+    const token = jwt.sign({
+        id: user.id,
+        username: user.username
+    }, process.env.SECRET, {expiresIn: '1d'})
+
+    res.status(200).json({token, username: user.username, id: user.id})
+}
+
+module.exports = {signUp, login}
---
+++ backend/controllers/userController.js
@@ -0,0 +1,50 @@
+const User = require('../models/User')
+
+
+const getUser = async(req, res) => {
+    const users = await User.find({})
+
+    return res.status(200).json(users)
+}
+
+const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user) return res.status(404).json({error: 'No User Found'})
+        res.status(200).json(user)
+    }
+    catch(error) {
+        res.status(400).json({error: 'Unknown Error'})
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const {username, name} = req.body
+        const user = await User.findByIdAndUpdate(req.params.id, {username, name}, {new: true})
+        if (!user) return res.status(404).json({error: 'User Not Found'})
+        res.status(200).json(user)
+    }
+    catch(error) {
+        res.status(400).json({error: 'Unknown Error'})
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) return res.status(404).json({error: 'No user Found'})
+        
+        res.status(200).json({message: 'User Deleted Successfully'})
+    }
+    catch (error) {
+        res.status(400).json({error: 'Delete Failed'})
+    }
+}
+
+module.exports = {
+    getUser, 
+    getUserById, 
+    updateUser,
+    deleteUser
+}
