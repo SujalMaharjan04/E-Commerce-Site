5413286fda6f2db189e459a38d8572df67353c60
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-09-30 23:27:01 +0545

    configured to use route

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./utils/loggers')
+const userRouter = require('./Routes/userRoute')
+const productRouter = require('./Routes/productRoute')
 
 const app = express()
+app.use(express.json())
 
 mongoose
     .connect(process.env.MONGODB_URL)
@@ -14,6 +17,9 @@ mongoose
         logger.error('Error connecting to the database', error.message)
 })
 
+
+app.use('/users', userRouter)
+app.use('/products', productRouter)
 
 
 module.exports = app
