4b857218fbe78a36ed5b2087ee9e12e3d397806a
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-10-08 23:25:22 +0545

    Product Controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,3 +5,25 @@ const getProduct = async (req, res) => {
     res.status(200).json(products)
 }
 
+
+const getProductById = async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (!product) {
+        return res.status(404).json({error: "Product Not Found!"})
+    }
+    res.status(200).json(product)
+}
+
+const getProductByCategory = async (req, res) => {
+    const {category} = req.body
+    const product = await Product.find({category})
+
+    if (!product) {
+        return res.status(404).json({error: 'Product Not Found'})
+    }
+
+    return res.status(200).json(product)
+}
+
+
