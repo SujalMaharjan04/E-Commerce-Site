5f95df9c3856ef8008d1988fa4799e703cf2c1eb
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-11-25 18:09:01 +0545

    Removed ScreenSize as display was already there

--- backend/controllers/seedProduct.js
+++ backend/controllers/seedProduct.js
@@ -13,7 +13,6 @@ const seedProduct = async () => {
             storage: "512GB SSD",
             display: "13.3-inch FHD",
             graphics: "Intel Iris Xe",
-            screenSize: "15.6inch"
             },
             category: "68f91ccaa0f8dd97e5b3439e", // replace with actual Category ObjectId
             brand: "68f91d1fa0f8dd97e5b343b0", // replace with actual Brand ObjectId
