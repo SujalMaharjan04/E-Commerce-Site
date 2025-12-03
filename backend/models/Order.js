ab9cc302c21830c8c89f3a28eddcc3eee8e45573
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-03 23:24:43 +0545

    fix: Added specs in items

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -6,6 +6,7 @@ const orderSchema = new mongoose.Schema({
         {
             product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
             quantity: {type: Number, required: true},
+            specs: {type: Object, default: {}, required: true},
             price: {type: Number, required: true}
         }
     ],
