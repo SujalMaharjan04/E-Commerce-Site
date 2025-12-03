f93d60725c4b68059721fb4303b7607ec3234d06
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-03 23:27:05 +0545

    Feature: Cart Context

---
+++ frontend/src/context/cartContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useReducer } from "react";
+
+const cartReducer = (state, action) => {
+    switch (action.type) {
+        case "SET_CART":
+            return action.payload
+
+        default: 
+            return state
+    }
+}
+
+
+const CartContext = createContext()
+
+const CartContextProvider = (props) => {
+    const [cart, dispatchCart] = useReducer(cartReducer, {user: null, items: []})
+
+    return (
+        <CartContext.Provider value = {[cart, dispatchCart]}>
+            {props.children}
+        </CartContext.Provider>
+    )
+}
+
+export { CartContextProvider, CartContext}
