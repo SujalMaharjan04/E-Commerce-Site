349f829673ad9179cd34b1c2cd6550f570a35528
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-03 23:25:40 +0545

    fix: Added Notification context

--- frontend/src/layout/UserLayout.jsx
+++ frontend/src/layout/UserLayout.jsx
@@ -1,11 +1,25 @@
 import Navbar from "../components/user/Navbar"
 import Footer from '../components/user/Footer'
+import { useContext } from "react"
 import { Outlet } from "react-router-dom"
+import { NotificationContext } from "../context/NotificationContext"
 
 const UserLayout = () => {
+    const [notification, dispatch] = useContext(NotificationContext)
+    
+    const baseStyle = "fixed top-5 right-5 px-4 py-2 rounded shadow-lg text-white font-medium transition-opacity duration-500";
+    const typeStyles = {
+        success: "bg-green-500",
+        error: "bg-red-500",
+    };
     return (
         <div className = "flex flex-col min-h-screen">
             <Navbar />
+                {notification && (
+                    <div className = {`${baseStyle} ${typeStyles[notification.type]}`}>
+                        {notification.text}
+                    </div>
+                )}
                 <main className = "grow bg-[#EFEBCE]"><Outlet /></main>
             <Footer />
         </div>
