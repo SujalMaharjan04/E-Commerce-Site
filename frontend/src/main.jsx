8288f3d61c75407f32c2853fd8c7c6fe6257c125
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-03 23:26:41 +0545

    Feature: Added Context for Cart

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { UserContextProvider, BrandContextProvider, CategoryContextProvider, ProductContextProvider, UsersContext, UsersContextProvider } from './context/adminContext.jsx'
 import {NotificationContextProvider } from './context/NotificationContext.jsx'
+import {CartContextProvider} from './context/cartContext.jsx'
 
 const queryClient = new QueryClient()
 
@@ -16,12 +17,14 @@ createRoot(document.getElementById('root')).render(
         <UserContextProvider>
           <ProductContextProvider>
             <BrandContextProvider>
-            <CategoryContextProvider>
-                <QueryClientProvider client = {queryClient}>
-                  <Router>
-                    <App />
-                  </Router>
-                </QueryClientProvider>
+              <CategoryContextProvider>
+                <CartContextProvider>
+                  <QueryClientProvider client = {queryClient}>
+                    <Router>
+                      <App />
+                    </Router>
+                  </QueryClientProvider>
+                </CartContextProvider>
               </CategoryContextProvider>
             </BrandContextProvider>
           </ProductContextProvider>
