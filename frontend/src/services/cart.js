f4398c7c14afe1fce1dc26de6f2a233f7e5c983d
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-03 23:28:09 +0545

    Feature: Added Function to get all cart items

--- frontend/src/services/cart.js
+++ frontend/src/services/cart.js
@@ -2,12 +2,18 @@ import api from '../services/api'
 
 const baseUrl = '/cart/users'
 
+//Get All Cart Item
+const getAllCart = async () => {
+    const response = await api.get(baseUrl)
+    
+    return response.data
+}
+
 //Add to Cart
 const addToCart = async (productId, quantity, selectedSpecs) => {
-    console.log(selectedSpecs)
     const response = await api.post(baseUrl, {productId, quantity, selectedSpecs})
     return response.data
 }
 
 
-export default {addToCart}
+export default {getAllCart, addToCart}
