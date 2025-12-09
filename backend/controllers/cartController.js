60f48302816fa541acbdbf956ab3589e35fc33e1
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-10 00:34:07 +0545

    fix & feature: Fixed the issue relating to naming & added the delete function

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -4,7 +4,7 @@ const Product = require('../models/Product')
 //Controller to Get Cart Items based on Logged In Users
 const getItems = async (req, res) => {
     try {
-        const cartItems = await Cart.findOne({user: req.user.id}).populate('user', 'name email').populate('items.products', 'image name price')
+        const cartItems = await Cart.findOne({user: req.user.id}).populate('user', 'name email').populate('items.product', 'image name price')
         
         if (!cartItems) {
             return res.status(404).json({items: []})
@@ -30,7 +30,7 @@ const addItems = async (req, res) => {
 
         if (product.stock < quantity) return res.status(400).json({error: "Not enough stock available"})
 
-        const cart = await Cart.findOne({user: req.user.id})
+        let cart = await Cart.findOne({user: req.user.id})
 
         if (!cart) {
             cart = new Cart({user: req.user.id, items: []})
@@ -39,7 +39,7 @@ const addItems = async (req, res) => {
         //Checking if product and specs already exists
         const existingItemIndex = cart.items.findIndex(
             item => 
-                item.products.toString() === productId && JSON.stringify(item.selectedSpecs) === JSON.stringify(selectedSpecs)
+                item.product.toString() === productId && JSON.stringify(item.selectedSpecs) === JSON.stringify(selectedSpecs)
         )
         
         if (existingItemIndex > -1) {
@@ -57,4 +57,28 @@ const addItems = async (req, res) => {
     }
 }
 
-module.exports = {getItems, addItems}
+
+//Controller for Deleting the cart item
+const deleteFromCart = async(req, res) => {
+    try {
+        const {productId} = req.body
+
+        const cartItem = await Cart.findOneAndUpdate(
+            {user: req.user.id},
+            {$pull: {items: {product: productId}}},
+            { new: true}
+        )
+
+        if (!cartItem) {
+            return res.status(404).json({error: "Item Not Found"})
+        }
+
+        return res.status(200).json(cartItem)
+    }
+
+    catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {getItems, addItems, deleteFromCart}
