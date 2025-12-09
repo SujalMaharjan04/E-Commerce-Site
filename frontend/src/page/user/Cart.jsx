c54959405394572c47966b70c854470c3de05869
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-09 22:39:15 +0545

    fix: Completed the UI portion of the Cart Page

--- frontend/src/page/user/Cart.jsx
+++ frontend/src/page/user/Cart.jsx
@@ -1,7 +1,8 @@
 import CartItems from '../../components/user/CartItems'
+import CartIcon from '../../assets/icons/image3.svg'
+import { useNavigate } from 'react-router-dom'
 const Cart = () => {
-
-
+    const navigate = useNavigate()
     return (
         <div className = "text-[#090F13] relative flex justify-around items-start gap-10">
             <div className = "w-[50%] space-y-4 flex flex-col">
@@ -22,12 +23,23 @@ const Cart = () => {
 
                 <hr className = "w-full border-4 rounded-lg border-white" />
 
-                <CartItems />
+                <div className = "min-h-64">
+                    <CartItems />
+                </div>
+
+                <hr className = "w-full border-4 rounded-lg border-white" />
+
+                <div className = "flex justify-end items-center mt-8">
+                    <button className = "bg-[#E09F75] w-60 rounded-lg h-14 flex items-center justify-center gap-8 hover:cursor-pointer">
+                        <img src = {CartIcon} alt = "cart Icon" className = "h-8 w-8" />
+                        <h2 className = "font-medium text-xl">Update Cart</h2>
+                    </button>
+                </div>
 
 
             </div>
 
-            <hr className = "fixed top-[35%] left-[50%] rotate-90 border-4 rounded-lg border-white w-[25%]" />
+            {/* <hr className = "fixed top-[50%] left-[62%] rotate-90 border-4 rounded-lg border-white w-[50%] -translate-x-1/2 -translate-y-1/2" /> */}
 
             <div className = "w-[25%] space-y-4">
                 <div>
@@ -42,6 +54,31 @@ const Cart = () => {
                 </div>
 
                 <hr className = "w-full border-4 rounded-lg border-white" />
+
+                <div className = "grid grid-cols-2 gap-5 my-10">
+                    <h3 className = "row-span-3">Shipping</h3>
+                    <div className = "flex items-center gap-2">
+                        Free Shipping <input type = "radio" />
+                    </div>
+                    <div className = "flex items-center gap-2">
+                        Free Shipping <input type = "radio" />
+                    </div>
+                    <div className = "flex items-center gap-2">
+                        Free Shipping <input type = "radio" />
+                    </div>
+                </div>
+
+                <hr className = "w-full border-4 rounded-lg border-white" />
+
+                <div className = "flex justify-between items-center py-2">
+                    <h3>Total</h3>
+                    <h3>Rs 3,99,999</h3>
+                </div>
+
+                <div className = "flex justify-center items-center mt-10">
+                    <button className = "bg-[#E09F75] w-full h-10 rounded-lg font-bold text-xl hover:cursor-pointer" onClick = {() => navigate("/order")}>Proceed To Checkout</button>
+                </div>
+
 
             </div>
 
