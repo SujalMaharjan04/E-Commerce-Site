1c41486c88fcf7d12288875165633a7267afd372
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-13 18:31:21 +0545

    feature: New Order Context for storing order details between order and payment page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { UserContextProvider, BrandContextProvider, CategoryContextProvider, ProductContextProvider, UsersContext, UsersContextProvider } from './context/adminContext.jsx'
 import {NotificationContextProvider } from './context/NotificationContext.jsx'
 import {CartContextProvider} from './context/cartContext.jsx'
+import { OrderContextProvider } from './context/orderContext.jsx'
 
 const queryClient = new QueryClient()
 
@@ -19,11 +20,13 @@ createRoot(document.getElementById('root')).render(
             <BrandContextProvider>
               <CategoryContextProvider>
                 <CartContextProvider>
-                  <QueryClientProvider client = {queryClient}>
-                    <Router>
-                      <App />
-                    </Router>
-                  </QueryClientProvider>
+                  <OrderContextProvider>
+                    <QueryClientProvider client = {queryClient}>
+                      <Router>
+                        <App />
+                      </Router>
+                    </QueryClientProvider>
+                  </OrderContextProvider>
                 </CartContextProvider>
               </CategoryContextProvider>
             </BrandContextProvider>
