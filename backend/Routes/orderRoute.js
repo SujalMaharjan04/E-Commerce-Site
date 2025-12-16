3df4b12845b4efa857f667331bf20eab1fe6ec01
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-16 13:42:21 +0545

    feature: Checking session for add order function

--- backend/Routes/orderRoute.js
+++ backend/Routes/orderRoute.js
@@ -1,4 +1,4 @@
-const { tokenExtractor, userExtractor } = require('../utils/middleware')
+const { tokenExtractor, userExtractor, checkSessionForCheckout } = require('../utils/middleware')
 const orderController = require('../controllers/orderController')
 const orderRouter = require('express').Router()
 
@@ -9,7 +9,7 @@ orderRouter.get('/', tokenExtractor, userExtractor, orderController.getOrders)
 orderRouter.get('/:id', tokenExtractor, userExtractor, orderController.getOneOrder)
 
 //Route to add New Order
-orderRouter.post('/', tokenExtractor, userExtractor, orderController.addOrder)
+orderRouter.post('/', tokenExtractor, userExtractor, checkSessionForCheckout, orderController.addOrder)
 
 //Route to update Order
 orderRouter.put('/:id', tokenExtractor, userExtractor, orderController.updateOrder)
