3e8654e62fd1e7f3d29e2af6e2376a5d8bdb35cd
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-17 19:36:29 +0545

    feature: cors function

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,10 @@ const cartRouter = require('./Routes/cartRoute')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use(session({
     name: 'sid',
