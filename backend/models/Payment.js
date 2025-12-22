a1aaeb7411ba1805535d65516e405eb09c5239b8
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-12-22 22:17:50 +0545

    fix: Added pidx, detailed transactionId and amount and added dataFormVerificationReq

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose')
 const paymentSchema = new mongoose.Schema({
     order: {type: mongoose.Schema.Types.ObjectId, ref: 'Order'},
     method: {type: String, enum: ['esewa', 'khalti', 'cod']},
-    transactionId: String,
-    amount: Number,
+    transactionId: {type: String, unique: true},
+    pidx: {type: String, unique: true},
+    amount: {type: Number, required: true},
     status: {type: String, enum: ['pending', 'completed', 'failed'], default: 'pending'},
+    dataFormVerficationReq: {type: Object},
     createdAt: {type: Date, default: Date.now}
 })
 
