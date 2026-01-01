20a8912b8a590a51b8d86d9b6cf7552b94ae7943
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-01-01 13:11:59 +0545

    feature: Separate Verification function for esewa

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -18,39 +18,44 @@ const initiatePayment = async(req, res, next) => {
 
         const amount = order.totalAmount
 
+        let transaction_uuid
+        if (paymentMethod === 'Esewa') {
+            transaction_uuid = uuidv4()
+        }
         const payment = new Payment({
             order: orderId,
             paymentMethod,
             amount,
+            transaction_uuid,
             status: 'pending'
         })
 
         await payment.save()
 
-        if (paymentMethod === 'Khalti') {
-            const payload = {
-                return_url: 'http://localhost:3001/api/payment/verify',
-                website_url: 'http://localhost:3001',
-                amount: amount * 100,
-                purchase_order_id: orderId,
-                purchase_order_name: "Order Payment" 
-            }
+        // if (paymentMethod === 'Khalti') {
+        //     const payload = {
+        //         return_url: 'http://localhost:3001/api/payment/verify',
+        //         website_url: 'http://localhost:3001',
+        //         amount: amount * 100,
+        //         purchase_order_id: orderId,
+        //         purchase_order_name: "Order Payment" 
+        //     }
 
-            const response = await axios.post('https://dev.khalti.com/api/v2/epayment/initiate', payload, {
-                headers: {Authorization: `Key ${config.KHALTI_SECRET_KEY}`}
-            })
+        //     const response = await axios.post('https://dev.khalti.com/api/v2/epayment/initiate', payload, {
+        //         headers: {Authorization: `Key ${config.KHALTI_SECRET_KEY}`}
+        //     })
 
-            paymentData = {
-                paymentId: payment._id,
-                paymentUrl: response.data.payment_url,
-                pidx: response.data.pidx,
-                provider: 'Khalti'
-            };
-        } else if (paymentMethod === 'Esewa') {
+        //     paymentData = {
+        //         paymentId: payment._id,
+        //         paymentUrl: response.data.payment_url,
+        //         pidx: response.data.pidx,
+        //         provider: 'Khalti'
+        // //     };
+        // } else 
+        if (paymentMethod === 'Esewa') {
             const esewaUrl = "https://rc-epay.esewa.com.np"
-            const successUrl = "http://localhost:3001/api/payment/verify"
-            const failureUrl = "http://localhost:3001/api/payment/failure"
-            const transaction_uuid = uuidv4();
+            
+            const verifyUrl = `http://localhost:3001/api/payment/verify/esewa`
 
             const message = `total_amount=${amount},transaction_uuid=${transaction_uuid},product_code=${config.ESEWA_MERCHANT_CODE}`
             const secretKey = config.ESEWA_SECRET_KEY
@@ -66,8 +71,8 @@ const initiatePayment = async(req, res, next) => {
                     amount: order.totalAmount,
                     product_service_charge: 0,
                     product_delivery_charge: 0,
-                    failure_url: failureUrl,
-                    success_url: successUrl,
+                    failure_url: verifyUrl,
+                    success_url: verifyUrl,
                     tax_amount: 0,
                     total_amount: order.totalAmount,
                     transaction_uuid: transaction_uuid,
@@ -115,50 +120,50 @@ const verifyPayment = async(req, res, next) => {
             payment.status = 'failed'
             await payment.save()
             return res.status(400).json({error: 'Khalti Payment Failed'})
-        } else if (paymentMethod === 'Esewa') {
-            const {oid, amt, refId, signed_field_name, signature} = req.body
-            const url = `https://rc.esewa.com.np/api/epay/transaction/status/?product_code=${config.ESEWA_MERCHANT_CODE}&total_amount=${amt}&transaction_uuid=${oid}`
-            const order = await Order.findById(oid)
-            if (!order) {
-                return res.status(404).json({error: "No Order  Found"})
-            }
+        // } else if (paymentMethod === 'Esewa') {
+        //     const {oid, amt, refId, signed_field_name, signature} = req.body
+        //     const url = `https://rc.esewa.com.np/api/epay/transaction/status/?product_code=${config.ESEWA_MERCHANT_CODE}&total_amount=${amt}&transaction_uuid=${oid}`
+        //     const order = await Order.findById(oid)
+        //     if (!order) {
+        //         return res.status(404).json({error: "No Order  Found"})
+        //     }
 
-            const message = `total_amount=${order.totalAmount},tansaction_uuid=${order._id},product_code=${config.ESEWA_MERCHANT_CODE}`
-            const hmac = crypto.createHmac('sha1', config.ESEWA_MERCHANT_CODE)
-            hmac.update(message)
-            const expectedSignature = hmac.digest('base64')
+        //     const message = `total_amount=${order.totalAmount},tansaction_uuid=${order._id},product_code=${config.ESEWA_MERCHANT_CODE}`
+        //     const hmac = crypto.createHmac('sha1', config.ESEWA_MERCHANT_CODE)
+        //     hmac.update(message)
+        //     const expectedSignature = hmac.digest('base64')
 
-            if (signature !== expectedSignature) {
-                return res.status(400).json({error: 'Payment Verification Failed'})
-            }
+        //     if (signature !== expectedSignature) {
+        //         return res.status(400).json({error: 'Payment Verification Failed'})
+        //     }
 
-            const params = new URLSearchParams({
-                amt, 
-                rid: refId,
-                pid: oid,
-                scd: config.ESEWA_MERCHANT_CODE
-            })
+        //     const params = new URLSearchParams({
+        //         amt, 
+        //         rid: refId,
+        //         pid: oid,
+        //         scd: config.ESEWA_MERCHANT_CODE
+        //     })
 
-            const response = await axios.post(url, params)
+        //     const response = await axios.post(url, params)
 
-            if (response.data.includes("<response_code>Success</response_code>")) {
-                const payment = await Payment.findOne({order: oid})
+        //     if (response.data.includes("<response_code>Success</response_code>")) {
+        //         const payment = await Payment.findOne({order: oid})
                 
-                if (!payment ) {
-                    return res.status(404).json({error: "No Payment Found"})
-                }
+        //         if (!payment ) {
+        //             return res.status(404).json({error: "No Payment Found"})
+        //         }
 
-                if (amt === order.totalAmount) {
-                    payment.status = 'completed'
-                    order.paymentStatus = 'paid'
-                    await payment.save()
-                    await order.save()
-                    return res.status(200).json({message: 'Esewa Payment Verified'})
-                }
-                else {
-                    return res.status(400).json({error: 'Amount and Order Total Amount doesnot match'})
-                }
-            }
+        //         if (amt === order.totalAmount) {
+        //             payment.status = 'completed'
+        //             order.paymentStatus = 'paid'
+        //             await payment.save()
+        //             await order.save()
+        //             return res.status(200).json({message: 'Esewa Payment Verified'})
+        //         }
+        //         else {
+        //             return res.status(400).json({error: 'Amount and Order Total Amount doesnot match'})
+        //         }
+        //     }
             return res.status(400).json({error: "Esewa Payment Failed"})
         }
     }
@@ -168,4 +173,70 @@ const verifyPayment = async(req, res, next) => {
     }
 }
 
-module.exports = {initiatePayment,verifyPayment}
+const verifyEsewa = async (req, res, next) => {
+    try {
+        const FRONTEND_URL = 'http://localhost:5173'
+        const {data} = req.query
+
+        if (!data) {
+            return res.status(400).json({error: "No Data"})
+        }
+
+        let decodedText = Buffer.from(data, 'base64').toString('utf-8')
+        decodedText = await JSON.parse(decodedText)
+        let headerList = {
+            Accept: "application/json",
+            "Content-type": "application/json"
+        }
+
+        const message = `transaction_code=${decodedText.transaction_code},status=${decodedText.status},total_amount=${decodedText.total_amount},transaction_uuid=${decodedText.transaction_uuid},product_code=${process.env.ESEWA_MERCHANT_CODE},signed_field_names=${decodedText.signed_field_names}`
+
+        const payment = await Payment.findOne({transaction_uuid: decodedText.transaction_uuid})
+        if (!payment) {
+            return res.status(404).json({error: "No Payment Found"})
+        }
+
+        const order = await Order.findById(payment.order)
+        if (!order) {
+            return res.status(404).json({error: "No Order Found"})
+        }
+
+        const secretKey = config.ESEWA_SECRET_KEY
+        const hash = crypto.createHmac("sha256", secretKey).update(message).digest('base64')
+        const url = `https://rc.esewa.com.np/api/epay/transaction/status/?product_code=${config.ESEWA_MERCHANT_CODE}&total_amount=${decodedText.total_amount}&transaction_uuid=${decodedText.transaction_uuid}`
+        let reqOption = {
+            url: url,
+            method: "GET",
+            headers: headerList
+        }
+
+        if (hash !== decodedText.signature) {
+            throw {message: "Invalid Info", decodedText}
+        }
+
+        let response = await axios.request(reqOption)
+
+        if (response.data.status === 'COMPLETE') {
+            payment.status = 'completed'
+            order.paymentStatus = 'paid'
+            await payment.save()
+            await order.save()
+
+            return res.redirect(`${FRONTEND_URL}/success`)
+        }
+
+        payment.status = 'failed'
+        order.paymentStatus = 'failed'
+        await payment.save()
+        await order.save()
+
+        return res.redirect(`${FRONTEND_URL}/failed`)
+        
+    }
+    catch (error) {
+        console.log('Error: ' + error.message)
+        next(error)
+    }
+}
+
+module.exports = {initiatePayment,verifyPayment, verifyEsewa}
