4db17c3b624fb100753c70893eb51544709aa1b6
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-01-02 21:00:35 +0545

    feature: New sidebar for all product page

---
+++ frontend/src/components/user/Sidebar.jsx
@@ -0,0 +1,73 @@
+
+const Sidebar = () => {
+    return (
+        <div className = "md:bg-[#BFC7E2] md:max-w-[20%] md:min-h-screen md:flex md:flex-col md:justify-evenly md:items-start md:ml-20 md:pl-20">
+            <div>
+                <h2 className="text-xl">Brand</h2>
+                <ul className = "list-disc list-inside text-xs">
+                    <li>Apple</li>
+                    <li>Lenovo</li>
+                    <li>Dell</li>
+                    <li>Asus</li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="text-xl">Brand</h2>
+                <ul className = "list-disc list-inside text-xs">
+                    <li>Apple</li>
+                    <li>Lenovo</li>
+                    <li>Dell</li>
+                    <li>Asus</li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="text-xl">Brand</h2>
+                <ul className = "list-disc list-inside text-xs">
+                    <li>Apple</li>
+                    <li>Lenovo</li>
+                    <li>Dell</li>
+                    <li>Asus</li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="text-xl">Brand</h2>
+                <ul className = "list-disc list-inside text-xs">
+                    <li>Apple</li>
+                    <li>Lenovo</li>
+                    <li>Dell</li>
+                    <li>Asus</li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="text-xl">Brand</h2>
+                <ul className = "list-disc list-inside text-xs">
+                    <li>Apple</li>
+                    <li>Lenovo</li>
+                    <li>Dell</li>
+                    <li>Asus</li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="text-xl">Brand</h2>
+                <ul className = "list-disc list-inside text-xs">
+                    <li>Apple</li>
+                    <li>Lenovo</li>
+                    <li>Dell</li>
+                    <li>Asus</li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="text-xl">Brand</h2>
+                <ul className = "list-disc list-inside text-xs">
+                    <li>Apple</li>
+                    <li>Lenovo</li>
+                    <li>Dell</li>
+                    <li>Asus</li>
+                </ul>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Sidebar
