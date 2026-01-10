bec6114ffe841e93c2d225e7d4b3036abaae4a67
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-01-10 23:25:30 +0545

    feature: Individual Product Card Component

---
+++ frontend/src/components/user/ProductPageCard.jsx
@@ -0,0 +1,23 @@
+
+
+const ProductPageCard = ({product}) => {
+    return (
+        <div className = "md:grid md:grid-cols-[auto_1fr] md:min-h-60 md:my-2 bg-[#BFC7E2] text-[#090F13]">
+            <div className = "bg-[#BFC7E2] md:mx-16 md:my-4  md:w-full  md:min-h-32 md:shadow-[-8px_-8px_12px_2px_rgba(0,0,0,0.25),8px_8px_12px_2px_rgba(0,0,0,0.25)] md:flex md:justify-center md:items-center">
+                <img src = {product.image[0]} alt = "Image 1" className = "md:h-48 md:w-48" />
+            </div>
+
+            <div className = "md:flex md:flex-col md:justify-center md:items-start md:ml-20 md:min-h-48">
+                <div>
+                    <p className = "md:leading-6 md:min-h-12">{product.name}</p>
+                    <p>Rs {product.price}</p>
+                </div>
+                <div className = "md:w-full">
+                    <button type = "button" className = "bg-[#E09F75] md:w-[20%] md:h-[20%] text-center md:my-14  rounded-lg font-bold text-lg">Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductPageCard
