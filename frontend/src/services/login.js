faebba0c204df13306f00bb26da2b24815713427
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-01-12 21:45:14 +0545

    feature: New logic to return the message when status code is 429 for rate limit

--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -3,14 +3,44 @@ import axios from 'axios'
 const API_URL = 'http://localhost:3001/api'
 
 const adminLogin = async(credentials) => {
-    const response = await axios.post(`${API_URL}/auth/login/admin`, credentials)
-    return response.data
+    try {
+        const response = await axios.post(`${API_URL}/auth/login/admin`, credentials)
+        return response.data
+    }
+    catch (err) {
+        
+        if (err.response) {
+            if (err.response.status === 429) {
+                return {
+                    success: false,
+                    type: 'RATE-LIMIT',
+                    message: err.response.message,
+                    retryAfter: err.response.retryAfter
+                }
+            }
+        }
+    }
 }
 
 
 const userLogin = async(credentials) => {
-    const response = await axios.post(`${API_URL}/auth/login/user`, credentials)
-    return response.data
+    try {
+        const response = await axios.post(`${API_URL}/auth/login/user`, credentials)
+        return response.data
+    }
+    catch (err) {
+        if (err.response) {
+            
+            if (err.response.status === 429) {
+                return {
+                    success: false,
+                    type: 'RATE-LIMIT',
+                    message: err.response.data.message,
+                    retryAfter: err.response.data.retryAfter
+                }
+            }
+        }
+    }
 }
 
 
