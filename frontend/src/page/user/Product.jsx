fa7fd309f65bd6c5128c06fd3a9676596a86bf05
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-01-17 21:12:37 +0545

    feature: product filter based on brands

--- frontend/src/page/user/Product.jsx
+++ frontend/src/page/user/Product.jsx
@@ -1,11 +1,12 @@
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import { useSearchParams } from "react-router-dom"
 import productService from '../../services/product'
 import Sidebar from "../../components/user/Sidebar"
 import ProductPageCard from "../../components/user/ProductPageCard"
 
 const Product = () => {
+    const [selectedBrand, setSelectedBrand] = useState('')
     const [searchParams] = useSearchParams()
     const category = searchParams.get("category")
     const {data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage} = useInfiniteQuery({
@@ -20,12 +21,16 @@ const Product = () => {
     if (isLoading) return (<h1>Loading....</h1>)
     if (isError) return (<h1>Error Getting Data</h1>)
     
-    const products = data.pages.flatMap(page => page.products)
-    
+    let products = data.pages.flatMap(page => page.products)
+
+    products = selectedBrand 
+        ? products.filter(p => p.brand === selectedBrand)
+        : products
+
     return (
         <div className = "md:flex md:flex-row md:justify-evenly ">
-            <div>
-                <Sidebar />
+            <div className = "bg-[#BFC7E2] md:min-w-[15%] md:h-fit md:ml-20 md:flex md:flex-col md:justify-evenly md:items-center">
+                <Sidebar selectedBrand = {selectedBrand} onBrandChange = {setSelectedBrand}/>
 
             </div>
             <div className = "md:flex md:flex-col md:flex-1 md:mx-10  ">
