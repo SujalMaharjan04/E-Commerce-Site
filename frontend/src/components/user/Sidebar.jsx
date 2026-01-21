bc9966f1a97b2c306fadb18ccf39a36811af0c41
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-01-21 12:55:06 +0545

    feature: Changes the brand only filter to object filter and added ram filtering

--- frontend/src/components/user/Sidebar.jsx
+++ frontend/src/components/user/Sidebar.jsx
@@ -3,7 +3,7 @@ import {BrandContext} from '../../context/adminContext'
 import { useQuery } from "@tanstack/react-query"
 import brandService from '../../services/brand'
 
-const Sidebar = ({selectedBrand, onBrandChange}) => {
+const Sidebar = ({searchFilter, onFilterChange}) => {
     const [brands, dispatchBrand] = useContext(BrandContext)
     
 
@@ -26,9 +26,29 @@ const Sidebar = ({selectedBrand, onBrandChange}) => {
                     {brands?.map(brand => (
                         
                         <div key = {brand.id}>
-                            <input  type = "radio" checked = {selectedBrand === brand.id} name = "brand" onChange = {() => onBrandChange(brand.id)} /> {brand.name}
+                            <input  type = "radio" value = {brand.id} checked = {searchFilter.brand === brand.id} name = "brand" onChange = {() => {}} onClick = {() => onFilterChange("brand", brand.id )} /> {brand.name}
                         </div>
                     ))}
+                </div>
+            </div>
+            <div>
+                <h2 className = "text-xl">RAM</h2>
+                <div className = "md:flex md:flex-col">
+                    <div>
+                        <input type = "radio" value = "4GB" checked = {searchFilter.ram === "4GB"} name = "ram" onChange = {() => {}} onClick = {() => onFilterChange("ram", "4GB")} /> 4GB
+                    </div>
+                    <div>
+                        <input type = "radio" value = "8GB" checked = {searchFilter.ram === "8GB"} name = "ram" onChange = {() => {}} onClick = {() => onFilterChange("ram", "8GB")} /> 8GB
+                    </div>
+                    <div>
+                        <input type = "radio" value = "16GB" checked = {searchFilter.ram === "16GB"} name = "ram" onChange = {() => {}} onClick = {() => onFilterChange("ram", "16GB")} /> 16GB
+                    </div>
+                    <div>
+                        <input type = "radio" value = "32GB" checked = {searchFilter.ram === "32GB"} name = "ram" onChange = {() => {}} onClick = {() => onFilterChange("ram", "32GB")} /> 32GB
+                    </div>
+                    <div>
+                        <input type = "radio" value = "64GB" checked = {searchFilter.ram === "64GB"} name = "ram" onChange = {() => {}} onClick = {() => onFilterChange("ram", "64GB")} /> 64GB
+                    </div>
                 </div>
             </div>
             
