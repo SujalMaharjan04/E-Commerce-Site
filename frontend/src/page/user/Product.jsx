39881eba7359917f5c3d1ef221da5ca804c46651
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-01-21 12:55:47 +0545

    feature: New FilterResolver for resolving the path of filter keys and filter logic to select the values in arrays for products

--- frontend/src/page/user/Product.jsx
+++ frontend/src/page/user/Product.jsx
@@ -6,7 +6,10 @@ import Sidebar from "../../components/user/Sidebar"
 import ProductPageCard from "../../components/user/ProductPageCard"
 
 const Product = () => {
-    const [selectedBrand, setSelectedBrand] = useState('')
+    const [searchFilter, setSearchFilter] = useState({
+        "brand": "",
+        "ram": ""
+    })
     const [searchParams] = useSearchParams()
     const category = searchParams.get("category")
     const {data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage} = useInfiniteQuery({
@@ -23,19 +26,49 @@ const Product = () => {
     
     let products = data.pages.flatMap(page => page.products)
 
-    products = selectedBrand 
-        ? products.filter(p => p.brand === selectedBrand)
-        : products
+    const handleFilter = (key, value) => {
+        setSearchFilter(prev => ({
+            ...prev,
+            [key]: prev[key] === value ? "" : value
+        }))
+    }
+
+    const filterResolver = {
+        brand: p => p.brand,
+        ram: p => p.specs?.ram
+    }
+
+    const hasValue = v => v !== "" && v != null
+
+
+    products = products.filter(p => 
+        Object.entries(searchFilter).every(([key, value]) => {
+            if (!hasValue(value)) return true
+            const resolver = filterResolver[key]
+
+            if (!resolver) return true
+
+            const productValue = resolver(p)
+
+            if (Array.isArray(productValue)) {
+                return productValue.some(v => String(v) === String(value))
+            }
+
+            return String(productValue) === String(value)
+        })
+    )
+
+    
 
     return (
         <div className = "md:flex md:flex-row md:justify-evenly ">
             <div className = "bg-[#BFC7E2] md:min-w-[15%] md:h-fit md:ml-20 md:flex md:flex-col md:justify-evenly md:items-center">
-                <Sidebar selectedBrand = {selectedBrand} onBrandChange = {setSelectedBrand}/>
+                <Sidebar searchFilter = {searchFilter} onFilterChange = {handleFilter}/>
 
             </div>
             <div className = "md:flex md:flex-col md:flex-1 md:mx-10  ">
                 {products.map(product => (
-                    <ProductPageCard product = {product} />
+                    <ProductPageCard key = {product.id} product = {product} />
                 ))}
             </div>
         </div>
