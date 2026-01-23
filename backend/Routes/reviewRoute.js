fe56b5b1f7773bfc74f062af88d9b44991d4f753
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-01-23 23:15:25 +0545

    fix: Removed the user check for get route of review

--- backend/Routes/reviewRoute.js
+++ backend/Routes/reviewRoute.js
@@ -3,7 +3,7 @@ const { tokenExtractor, userExtractor } = require('../utils/middleware')
 const reviewRouter = require('express').Router()
 
 //Route to get the Review
-reviewRouter.get('/:id/review', tokenExtractor, userExtractor, reviewController.getById)
+reviewRouter.get('/:id/review',  reviewController.getById)
 
 //Route to add a Review
 reviewRouter.post('/:id/review', tokenExtractor, userExtractor, reviewController.addReview)
