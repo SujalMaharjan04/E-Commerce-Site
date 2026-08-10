6c2f73731377d3842ccb8238cb62b92f78c50dc7
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-08-10 22:11:10 +0545

    refactor: Removed the notificationContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { UserContextProvider, BrandContextProvider, CategoryContextProvider, ProductContextProvider, UsersContext, UsersContextProvider } from './context/adminContext.jsx'
-import {NotificationContextProvider } from './context/NotificationContext.jsx'
 import {CartContextProvider} from './context/cartContext.jsx'
 import { OrderContextProvider } from './context/orderContext.jsx'
 import { ReviewContextProvider } from './context/reviewContext.jsx'
@@ -14,7 +13,6 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NotificationContextProvider>
       <UsersContextProvider>      
         <UserContextProvider>
           <ReviewContextProvider>
@@ -36,6 +34,5 @@ createRoot(document.getElementById('root')).render(
           </ReviewContextProvider>
         </UserContextProvider>
       </UsersContextProvider>
-    </NotificationContextProvider>
   </StrictMode>,
 )
