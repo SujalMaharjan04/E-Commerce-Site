bb3a6482cb700d22ee553bb36607218a6e44847a
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-08-14 20:06:26 +0545

    fix: Removed the token state and added isAuthenticated and isLoading and checkAuth for checking whether the token exists or not

--- frontend/src/store/auth.store.js
+++ frontend/src/store/auth.store.js
@@ -1,28 +1,39 @@
 import {create} from 'zustand' 
 import {createJSONStorage, persist} from 'zustand/middleware'
+import authService from '../services/auth'
 
 const useAuthStore = create(
     persist(
         (set) => ({
         //This stores the user info: e.g name, and other info
-        userInfo: {},
-        token: null, //This stores the token received during login or signup
+        user: null,
+        isAuthenticated: false,
+        isLoading: true,
 
-        //This sets the userinfo into the state
-        setUserInfo: (userInfo) => set({userInfo}),
-
-        //Stores the token
-        setToken: (token) => set({token}),
+        //This sets the user into the state
+        setUser: (user) => set({user, isAuthenticated: true}),
 
         //Clear the token when logging out
-        logout: () => set({userInfo: {}, token: null})
+        logout: async() => {
+            await authService.logout()
+            set({user: {}, isAuthenticated: false})
+        },
+
+        checkAuth: async() => {
+            try {
+                const user = await authService.checkAuth()
+                set({user, isAuthenticated: true, isLoading: false})
+            }
+            catch {
+                set({user: null, isAuthenticated: false, isLoading: false})
+            }
+        }
         }),
         {
             name: "loggedApp",
             storage: createJSONStorage(() => sessionStorage),
             partialize: (state) => ({
-                token: state.token,
-                userInfo: state.userInfo
+                user: state.user
             })
         }
     )
