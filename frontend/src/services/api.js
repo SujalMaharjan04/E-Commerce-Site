90fa2d6469657d893e832db0ad4360bed6ae53e0
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-08-14 20:08:29 +0545

    fix: Removed the api.interceptors previously used for setting the token to the header which is done via httpOnly cookie

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,24 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: "http://localhost:3001/api",
+    withCredentials: true
 })
 
-api.interceptors.request.use((config) => {
-    const stored = sessionStorage.getItem('loggedApp')
-    
-    if (stored) {
-        try {
-            const parsed = JSON.parse(stored)
-            const token = parsed?.state.token
-            if (token) {
-                config.headers.Authorization = `Bearer ${token}`
-        }}
-        catch (error) {
-            console.log(error.message)
-        }
-    }
-
-    return config
-})
 
 export default api
