bcaf6e58f4737f3d01cc2434a0b249df7eab3db9
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2026-08-25 13:54:29 +0545

    CORS config changes, and adding env variables

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const cookieParser = require('cookie-parser')
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://e-commerce-site-lyart-nine.vercel.app/',
     credentials: true
 }))
 app.use(cookieParser())
